c5123f098d6b3a34cf52430db9e27f446a042169
chenqingxiu <chenqingxiu0592@gmail.com>
2017-10-19 22:57:47 +0800

    1019

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
-// class ListContacts extends Component {
-//   render(){
-//     console.log('Props', this.props)
+// function ListContacts(props) {
 //     return (
 //       <ol className='contact-list'>
-//         {this.props.contacts.map((contact)=>(
+//         {props.contacts.map((contact)=>(
 //           <li key={contact.id} className='contact-list-item'>
 //             <div className='contact-avatar' style={{
 //               backgroundImage:`url(${contact.avatarURL})`}}>
@@ -14,7 +13,7 @@ import React, {Component} from 'react'
 //               <p>{contact.name}</p>
 //               <p>{contact.email}</p>
 //             </div>
-//             <button className='contact-remove'>
+//             <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
 //               remove
 //             </button>
 //           </li>
@@ -22,28 +21,55 @@ import React, {Component} from 'react'
 //       </ol>
 //       )
 //   }
-// }
 
+class ListContacts extends Component {
 
-function ListContacts(props) {
-    return (
-      <ol className='contact-list'>
-        {props.contacts.map((contact)=>(
-          <li key={contact.id} className='contact-list-item'>
-            <div className='contact-avatar' style={{
-              backgroundImage:`url(${contact.avatarURL})`}}>
-            </div>
-            <div className='contact-details'>
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-              remove
-            </button>
-          </li>
-          ))}
-      </ol>
-      )
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+}
+
+  state = {
+    query: ''
   }
+
+  updateQuery = (query) => {
+    this.setState({query: query.trim()})
+  }
+
+  render() {
+      return (
+        <div className='list-contacts'>
+          {JSON.stringify(this.state)}
+          <div className='list-contacts-top'>
+            <input
+              className='search-contacts'
+              type='text'
+             placeholder='Search contacts'
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
+         </div>
+         <ol className='contact-list'>
+           {this.props.contacts.map((contact) => (
+             <li key={contact.id} className='contact-list-item'>
+               <div className='contact-avatar' style={{
+                 backgroundImage: `url(${contact.avatarURL})`
+               }}/>
+               <div className='contact-details'>
+                 <p>{contact.name}</p>
+                 <p>{contact.email}</p>
+               </div>
+               <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                 Remove
+               </button>
+             </li>
+           ))}
+         </ol>
+       </div>
+     )
+   }
+
+}
 
 export default ListContacts
