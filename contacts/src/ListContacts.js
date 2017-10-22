cb72fc3dad8c9b01bd8d63b3091bfccb87e45d5a
chenqingxiu <chenqingxiu0592@gmail.com>
2017-10-22 22:26:24 +0800

    pic

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,27 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-// function ListContacts(props) {
-//     return (
-//       <ol className='contact-list'>
-//         {props.contacts.map((contact)=>(
-//           <li key={contact.id} className='contact-list-item'>
-//             <div className='contact-avatar' style={{
-//               backgroundImage:`url(${contact.avatarURL})`}}>
-//             </div>
-//             <div className='contact-details'>
-//               <p>{contact.name}</p>
-//               <p>{contact.email}</p>
-//             </div>
-//             <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-//               remove
-//             </button>
-//           </li>
-//           ))}
-//       </ol>
-//       )
-//   }
-
 class ListContacts extends Component {
 
   static propTypes = {
